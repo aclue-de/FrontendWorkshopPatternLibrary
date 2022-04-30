983931390344bfc456ee8a81b50901681b6bcca9
Robin Müller <robin.mueller@aclue.de>
2022-04-30 16:24:23 +0200

    Revert "clean repo"
    This reverts commit 455881973ac893892352917fd3ca69b68a94d25e.

---
+++ main.js
@@ -0,0 +1,11 @@
+import './src/components/NumberPad'
+import './src/components/RadialPicker'
+import './src/components/TouchSpinner'
+
+document.querySelector('#app').innerHTML = `
+  <div style="paddding: 32px; display: flex; flex-direction: column; align-content: center; gap: 32px;">
+    <number-pad minimum="0" maximum="10" value="1" title="Elemente"></number-pad>
+    <radial-picker></radial-picker>
+    <touch-spinner minimum="0" maximum="50" value="10" title="Elemente"></touch-spinner>
+  </div>
+`
