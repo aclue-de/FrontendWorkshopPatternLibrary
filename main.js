4b5785907daa71d8ca1eaacc793390049bcbfc1c
Robin Müller <robin.mueller@aclue.de>
2022-04-30 14:27:39 +0200

    align vertically

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import './src/components/RadialPicker'
 import './src/components/TouchSpinner'
 
 document.querySelector('#app').innerHTML = `
-  <div style="paddding: 32px; display: flex; align-content: center; gap: 32px;">
+  <div style="paddding: 32px; display: flex; flex-direction: column; align-content: center; gap: 32px;">
     <number-pad minimum="0" maximum="10" value="1" title="Elemente"></number-pad>
     <radial-picker></radial-picker>
     <touch-spinner minimum="0" maximum="50" value="10" title="Elemente"></touch-spinner>
